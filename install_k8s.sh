12f29de6ac3c5301a6c90f19be8d7bdce435fdae
macharya <madhav_acharya@hotmail.com>
2021-06-03 14:00:59 -0400

    update install with yum

--- install_k8s.sh
+++ install_k8s.sh
@@ -1,12 +1,12 @@
 sudo su -
-sudo apt-get update
-sudo apt-get install curl
-sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+sudo yum update
+sudo yum install curl
+sudo yum install -y apt-transport-https ca-certificates curl software-properties-common
 
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update ; clear
-sudo apt-get install -y docker-ce
+sudo yum update ; clear
+sudo yum install -y docker-ce
 sudo service docker start ; clear
 
 ### INSTALL KUBEADM,KUBELET,KUBECTL
@@ -14,8 +14,8 @@ sudo service docker start ; clear
 sudo su -
 sudo echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo apt-get update ; clear
-sudo apt-get install -y kubelet kubeadm kubectl	
+sudo yum update ; clear
+sudo yum install -y kubelet kubeadm kubectl	
 ### check the version
 
 docker -v  ;  echo "kubeadm version: "  'kubeadm version -o short';  echo " kubelet --version" 'kubelet --version' ; echo "kubectl version" ; kubectl version --short 
