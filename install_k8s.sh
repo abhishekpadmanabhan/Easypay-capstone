47f20560e8a8879b2c8c1b0d40b89f8ec5ffc580
macharya <madhav_acharya@hotmail.com>
2021-06-03 15:15:15 -0400

    update k8s install scrips

--- install_k8s.sh
+++ install_k8s.sh
@@ -1,21 +1,20 @@
 sudo su -
-sudo apt-get update
-sudo apt-get install curl
-sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+apt-get update
+apt-get install curl
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update ; clear
-sudo apt-get install -y docker-ce
-sudo service docker start ; clear
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update ; clear
+apt-get install -y docker-ce
+service docker start ; clear
 
 ### INSTALL KUBEADM,KUBELET,KUBECTL
 
-sudo su -
-sudo echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo apt-get update ; clear
-sudo apt-get install -y kubelet kubeadm kubectl	
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+apt-get update ; clear
+apt-get install -y kubelet kubeadm kubectl	
 ### check the version
 
 docker -v  ;  echo "kubeadm version: "  'kubeadm version -o short';  echo " kubelet --version" 'kubelet --version' ; 
