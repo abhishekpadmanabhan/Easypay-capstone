c606ddb042d35d696d17a3b1c5173d62efe33fde
macharya <madhav_acharya@hotmail.com>
2021-06-03 15:33:01 -0400

    maser

--- install_k8s_master.sh
+++ install_k8s_master.sh
@@ -1,8 +1,8 @@
-
-sudo kubeadm init
-sudo mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
+sudo su -
+kubeadm init
+mkdir -p $HOME/.kube
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config 
+chown $(id -u):$(id -g) $HOME/.kube/config
 ## Weave
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')" 
 kubectl get nodes
